ce776235ddec9ab7c91383cbce7575bdfd98b398
hmz <66381391+saqoah@users.noreply.github.com>
2021-07-18 23:29:23 +0200

    Update M3u.php

--- M3u.php
+++ M3u.php
@@ -1,7 +1,3 @@
 <?php
-echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+echo "#EXTINF:-1 tvg-id=\"" . $_GET[id] . "\" group-title=\"" . $_GET[group] . "\" tvg-logo="\" . $_GET[logo] . "\"," . $_GET[title]
 ?>
-
-#EXTM3U 
-#EXTINF:-1 tvg-id="HomeList" group-title="Group" tvg-logo="https://i.pinimg.com/736x/1b/4c/60/1b4c60f47895a0928f2567dec949d4a3.jpg",English Cartoon
-https://raw.githubusercontent.com/saqoah/data/main/aa.m3u
