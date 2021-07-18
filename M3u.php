65b869359e9fbf5021581577a6852d913bc9d31b
hmz <66381391+saqoah@users.noreply.github.com>
2021-07-18 23:30:27 +0200

    Update M3u.php

--- M3u.php
+++ M3u.php
@@ -1,3 +1,3 @@
 <?php
-echo "#EXTINF:-1 tvg-id=\"" . $_GET[id] . "\" group-title=\"" . $_GET[group] . "\" tvg-logo="\" . $_GET[logo] . "\"," . $_GET[title]
+echo "#EXTINF:-1 tvg-id=\"" . $_GET[id] . "\" group-title=\"" . $_GET[group] . "\" tvg-logo=\"" . $_GET[logo] . "\"," . $_GET[title]
 ?>
