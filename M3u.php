a6239945d538c11006ae1fab53e23dd31de968d3
hmz <66381391+saqoah@users.noreply.github.com>
2021-07-18 23:20:43 +0200

    Update and rename forkids.m3u to M3u.php

---
+++ M3u.php
@@ -0,0 +1,7 @@
+<?php
+echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+?>
+
+#EXTM3U 
+#EXTINF:-1 tvg-id="HomeList" group-title="Group" tvg-logo="https://i.pinimg.com/736x/1b/4c/60/1b4c60f47895a0928f2567dec949d4a3.jpg",English Cartoon
+https://raw.githubusercontent.com/saqoah/data/main/aa.m3u
