65076d36d963a5bc2771b61dedf8cf0af341d491
hmz <66381391+saqoah@users.noreply.github.com>
2021-07-19 17:18:03 +0200

    Update M3u.php

--- M3u.php
+++ M3u.php
@@ -2,5 +2,5 @@
 $res = '';
 $res = "#EXTINF:-1 tvg-id=\"" . $_GET[id] . "\" group-title=\"" . $_GET[group] . "\" tvg-logo=\"" . $_GET[logo] . "\"," . $_GET[title];
 echo $res;
-file_put_contents('free_playlist.m3u',$res,FILE_APPEND)
+file_put_contents('free_playlist.m3u',$res,FILE_APPEND);
 ?>
